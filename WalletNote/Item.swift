82b8801c3f34d43e45c1dac717f974e850e09ab0
ta17eee <r202304722ob@jindai.jp>
2025-02-04 15:56:58 +0900

    Initial Commit

---
+++ WalletNote/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WalletNote
+//
+//  Created by 高野　泰生 on 2025/02/04.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WalletNoteTests/WalletNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  WalletNoteTests.swift
+//  WalletNoteTests
+//
+//  Created by 高野　泰生 on 2025/02/04.
+//
+
+import Testing
+@testable import WalletNote
+
+struct WalletNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
