2e73caf1dbbe0a643859c72aefd86e46378249ed
ta17eee <ta17eee@gmail.com>
2025-02-17 18:59:17 +0900

    refactor: CashViewの現金のアイコン部分を分離

--- WalletNote/CashView.swift
+++ WalletNote/CashView.swift
@@ -21,15 +21,7 @@ struct CashView: View {
 //                    .frame(width: 16)
                 VStack {
                     HStack {
-                        ZStack {
-                            Rectangle()
-                                .fill(Color(red: 248/255, green: 181/255, blue: 0.0))
-                                .frame(width: 64, height: 32)
-                            Text("10000")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                                .foregroundStyle(.black)
-                        }
-                        .frame(width: 64, height: 32)
+                        Bill10000()
                         Spacer()
                             .frame(width: 8)
                         Text("× " + String(data.cashData[10000]!))
@@ -40,15 +32,7 @@ struct CashView: View {
                     Spacer()
                         .frame(height: 16)
                     HStack {
-                        ZStack {
-                            Rectangle()
-                                .fill(Color(red: 196/255, green: 163/255, blue: 191/255))
-                                .frame(width: 64, height: 32)
-                            Text("5000")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                                .foregroundStyle(.black)
-                        }
-                        .frame(width: 64, height: 32)
+                        Bill5000()
                         Spacer()
                             .frame(width: 8)
                         Text("× " + String(data.cashData[5000]!))
@@ -59,15 +43,7 @@ struct CashView: View {
                     Spacer()
                         .frame(height: 16)
                     HStack {
-                        ZStack {
-                            Rectangle()
-                                .fill(Color(red: 128/255, green: 171/255, blue: 169/255))
-                                .frame(width: 64, height: 32)
-                            Text("1000")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                                .foregroundStyle(.black)
-                        }
-                        .frame(width: 64, height: 32)
+                        Bill1000()
                         Spacer()
                             .frame(width: 8)
                         Text("× " + String(data.cashData[1000]!))
@@ -83,14 +59,7 @@ struct CashView: View {
                 Spacer()
                 VStack {
                     HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: 255/255, green: 217/255, blue: 0/255))
-                                .frame(width: 32, height: 32)
-                            Text("500")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                        }
-                        .frame(width: 32, height: 32)
+                        Coin500()
                         Spacer()
                             .frame(width: 8)
                         Text("× " + String(data.cashData[500]!))
@@ -101,14 +70,7 @@ struct CashView: View {
                     Spacer()
                         .frame(height: 16)
                     HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: 192/255, green: 198/255, blue: 201/255))
-                                .frame(width: 32, height: 32)
-                            Text("100")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                        }
-                        .frame(width: 32, height: 32)
+                        Coin100()
                         Spacer()
                             .frame(width: 8)
                         Text("x " + String(data.cashData[100]!))
@@ -119,13 +81,7 @@ struct CashView: View {
                     Spacer()
                         .frame(height: 16)
                     HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: 175/255, green: 175/255, blue: 176/255))
-                                .frame(width: 32, height: 32)
-                            Text("50")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                        }
+                        Coin50()
                         .frame(width: 32, height: 32)
                         Spacer()
                             .frame(width: 8)
@@ -142,13 +98,7 @@ struct CashView: View {
                 Spacer()
                 VStack {
                     HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: 184/255, green: 115/255, blue: 51/255))
-                                .frame(width: 32, height: 32)
-                            Text("10")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                        }
+                        Coin10()
                         Spacer()
                             .frame(width: 8)
                         Text("x " + String(data.cashData[10]!))
@@ -159,13 +109,7 @@ struct CashView: View {
                     Spacer()
                         .frame(height: 16)
                     HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: 200/255, green: 153/255, blue: 50/255))
-                                .frame(width: 32, height: 32)
-                            Text("5")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                        }
+                        Coin5()
                         Spacer()
                             .frame(width: 8)
                         Text("x " + String(data.cashData[5]!))
@@ -176,13 +120,7 @@ struct CashView: View {
                     Spacer()
                         .frame(height: 16)
                     HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color(red: 230/255, green: 230/255, blue: 250/255))
-                                .frame(width: 32, height: 32)
-                            Text("1")
-                                .font(.system(size: 16, weight: .bold, design: .default))
-                        }
+                        Coin1()
                         Spacer()
                             .frame(width: 8)
                         Text("x " + String(data.cashData[1]!))
@@ -198,6 +136,132 @@ struct CashView: View {
     }
 }
 
+struct CashInputView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+private struct Bill10000: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color(red: 248/255, green: 181/255, blue: 0.0))
+                .frame(width: 64, height: 32)
+            Text("10000")
+                .font(.system(size: 16, weight: .bold, design: .default))
+                .foregroundStyle(.black)
+        }
+        .frame(width: 64, height: 32)
+    }
+}
+
+private struct Bill5000: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color(red: 196/255, green: 163/255, blue: 191/255))
+                .frame(width: 64, height: 32)
+            Text("5000")
+                .font(.system(size: 16, weight: .bold, design: .default))
+                .foregroundStyle(.black)
+        }
+        .frame(width: 64, height: 32)
+    }
+}
+
+private struct Bill1000: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color(red: 128/255, green: 171/255, blue: 169/255))
+                .frame(width: 64, height: 32)
+            Text("1000")
+                .font(.system(size: 16, weight: .bold, design: .default))
+                .foregroundStyle(.black)
+        }
+        .frame(width: 64, height: 32)
+    }
+}
+
+private struct Coin500: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color(red: 255/255, green: 217/255, blue: 0/255))
+                .frame(width: 32, height: 32)
+            Text("500")
+                .font(.system(size: 16, weight: .bold, design: .default))
+        }
+        .frame(width: 32, height: 32)
+    }
+}
+
+private struct Coin100: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color(red: 192/255, green: 198/255, blue: 201/255))
+                .frame(width: 32, height: 32)
+            Text("100")
+                .font(.system(size: 16, weight: .bold, design: .default))
+        }
+        .frame(width: 32, height: 32)
+    }
+}
+
+private struct Coin50: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color(red: 175/255, green: 175/255, blue: 176/255))
+                .frame(width: 32, height: 32)
+            Text("50")
+                .font(.system(size: 16, weight: .bold, design: .default))
+        }
+        .frame(width: 32, height: 32)
+    }
+}
+
+private struct Coin10: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color(red: 184/255, green: 115/255, blue: 51/255))
+                .frame(width: 32, height: 32)
+            Text("10")
+                .font(.system(size: 16, weight: .bold, design: .default))
+        }
+        .frame(width: 32, height: 32)
+    }
+}
+
+private struct Coin5: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color(red: 200/255, green: 153/255, blue: 50/255))
+                .frame(width: 32, height: 32)
+            Text("5")
+                .font(.system(size: 16, weight: .bold, design: .default))
+        }
+        .frame(width: 32, height: 32)
+    }
+}
+
+private struct Coin1: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color(red: 230/255, green: 230/255, blue: 250/255))
+                .frame(width: 32, height: 32)
+            Text("1")
+                .font(.system(size: 16, weight: .bold, design: .default))
+        }
+        .frame(width: 32, height: 32)
+    }
+}
+
 #Preview {
     HStack {
         Spacer()
@@ -206,5 +270,13 @@ struct CashView: View {
         Spacer()
             .frame(width: 32)
     }
-    
+    Spacer()
+        .frame(height: 32)
+    HStack {
+        Spacer()
+            .frame(width: 32)
+        CashInputView()
+        Spacer()
+            .frame(width: 32)
+    }
 }
