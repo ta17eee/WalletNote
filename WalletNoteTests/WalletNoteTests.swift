ce283097a28cbc046b91bf4478e2269496654701
ta17eee <ta17eee@gmail.com>
2025-04-14 10:10:13 +0900

    test: WalletDataの加算・減算周りの単体テストを追加

--- WalletNoteTests/WalletNoteTests.swift
+++ WalletNoteTests/WalletNoteTests.swift
@@ -5,13 +5,41 @@
 //  Created by 高野　泰生 on 2025/02/04.
 //
 
-import Testing
+import XCTest
 @testable import WalletNote
 
-struct WalletNoteTests {
+final class WalletNoteTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    func testAddCash() {
+        var walletData = WalletData()
+        walletData.addCash(1000)
+        XCTAssertEqual(walletData.getCashAmount(1000), 1, "1000円札が1枚追加されるべきです")
     }
 
+    func testRemoveCash() {
+        var walletData = WalletData()
+        walletData.addCash(1000)
+        walletData.removeCash(1000)
+        XCTAssertEqual(walletData.getCashAmount(1000), 0, "1000円札が1枚減少するべきです")
+    }
+
+    func testPlus() {
+        var walletData1 = WalletData()
+        var walletData2 = WalletData()
+        walletData1.addCash(1000, 2) // 1000円札を2枚追加
+        walletData2.addCash(1000, 3) // 1000円札を3枚追加
+
+        let result = walletData1.plus(walletData2)
+        XCTAssertEqual(result.getCashAmount(1000), 5, "1000円札が合計5枚になるべきです")
+    }
+
+    func testMinus() {
+        var walletData1 = WalletData()
+        var walletData2 = WalletData()
+        walletData1.addCash(1000, 5) // 1000円札を5枚追加
+        walletData2.addCash(1000, 3) // 1000円札を3枚追加
+
+        let result = walletData1.minus(walletData2)
+        XCTAssertEqual(result.getCashAmount(1000), 2, "1000円札が合計2枚になるべきです")
+    }
 }
