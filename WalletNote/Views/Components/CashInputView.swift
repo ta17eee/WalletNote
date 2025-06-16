c254655d4381e835f1985eac87381527169ea271
ta17eee <ta17eee@gmail.com>
2025-06-16 11:42:32 +0900

    change: 現金で金額を入力するときに、所持している現金のみ利用可能になるように変更

--- WalletNote/Views/Components/CashInputView.swift
+++ WalletNote/Views/Components/CashInputView.swift
@@ -1,156 +1,102 @@
-
 import SwiftUI
 
 struct CashInputView: View {
+    @EnvironmentObject var dataContext: CentralDataContext
     @Binding var data: WalletData
-    
+    var remain: WalletData.CashData {
+        dataContext.walletData.minus(data).getCashData()
+    }
+
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 8)
                 .fill(Color(.tertiarySystemBackground))
-                .stroke(Color.gray, lineWidth: 2)
+                .shadow(radius: 2)
                 .frame(height: 196)
             HStack {
                 Spacer()
-                VStack {
-                    HStack {
-                        Button(action: {
-                            data.addCash(10000)
-                        }) {
-                            Bill10000()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(10000)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                VStack(spacing: 16) {
+                    CashButtonSet(amount: 10000, data: $data, remain: remain) {
+                        Bill10000()
                     }
-                    Spacer()
-                        .frame(height: 16)
-                    HStack {
-                        Button(action: {
-                            data.addCash(5000)
-                        }) {
-                            Bill5000()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(5000)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                    CashButtonSet(amount: 5000, data: $data, remain: remain) {
+                        Bill5000()
                     }
-                    Spacer()
-                        .frame(height: 16)
-                    HStack {
-                        Button(action: {
-                            data.addCash(1000)
-                        }) {
-                            Bill1000()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(1000)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                    CashButtonSet(amount: 1000, data: $data, remain: remain) {
+                        Bill1000()
                     }
                 }
                 Spacer()
-                VStack {
-                    HStack {
-                        Button(action: {
-                            data.addCash(500)
-                        }) {
-                            Coin500()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(500)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                VStack(spacing: 16) {
+                    CashButtonSet(amount: 500, data: $data, remain: remain) {
+                        Coin500()
                     }
-                    Spacer()
-                        .frame(height: 16)
-                    HStack {
-                        Button(action: {
-                            data.addCash(100)
-                        }) {
-                            Coin100()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(100)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                    CashButtonSet(amount: 100, data: $data, remain: remain) {
+                        Coin100()
                     }
-                    Spacer()
-                        .frame(height: 16)
-                    HStack {
-                        Button(action: {
-                            data.addCash(50)
-                        }) {
-                            Coin50()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(50)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                    CashButtonSet(amount: 50, data: $data, remain: remain) {
+                        Coin50()
                     }
                 }
                 Spacer()
-                VStack {
-                    HStack {
-                        Button(action: {
-                            data.addCash(10)
-                        }) {
-                            Coin10()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(10)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                VStack(spacing: 16) {
+                    CashButtonSet(amount: 10, data: $data, remain: remain) {
+                        Coin10()
                     }
-                    Spacer()
-                        .frame(height: 16)
-                    HStack {
-                        Button(action: {
-                            data.addCash(5)
-                        }) {
-                            Coin5()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(5)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                    CashButtonSet(amount: 5, data: $data, remain: remain) {
+                        Coin5()
                     }
-                    Spacer()
-                        .frame(height: 16)
-                    HStack {
-                        Button(action: {
-                            data.addCash(1)
-                        }) {
-                            Coin1()
-                        }
-                        .padding(.all, 6)
-                        Button(action: {
-                            data.removeCash(1)
-                        }) {
-                            RemoveCashIcon()
-                        }
+                    CashButtonSet(amount: 1, data: $data, remain: remain) {
+                        Coin1()
                     }
                 }
                 Spacer()
             }
         }
+    }
+}
+
+struct CashButtonSet<Content: View>: View {
+    let amount: Int
+    @Binding var data: WalletData
+    let remain: WalletData.CashData
+    let content: Content
+
+    init(amount: Int, data: Binding<WalletData>, remain: WalletData.CashData, @ViewBuilder content: () -> Content) {
+        self.amount = amount
+        self._data = data
+        self.remain = remain
+        self.content = content()
+    }
+
+    var body: some View {
+        HStack {
+            Button(action: {
+                data.addCash(amount)
+            }) {
+                content
+            }
+            .padding(.all, 6)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+            .opacity(remain.getCashAmount(amount) == 0 ? 0.25 : 1)
+            .shadow(radius: remain.getCashAmount(amount) == 0 ? 0 : 6)
+            .disabled(remain.getCashAmount(amount) <= 0)
+
+            Button(action: {
+                data.removeCash(amount)
+            }) {
+                RemoveCashIcon()
+            }
+        }
+    }
+}
+
+#Preview {
+    HStack {
+        Spacer()
+            .frame(width: 16)
+        CashInputView(data: .constant(WalletData()))
+        Spacer()
+            .frame(width: 16)
     }
 }
