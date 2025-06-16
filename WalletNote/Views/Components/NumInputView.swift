37a2a7b613402fa969f28ca6d15f75a4f1f2be84
ta17eee <ta17eee@gmail.com>
2025-06-16 11:39:25 +0900

    refactor: 表示の大きさの種別を管理するenumの名前をDisplaySizeに変更

---
+++ WalletNote/Views/Components/NumInputView.swift
@@ -0,0 +1,18 @@
+//
+//  NumInputView.swift
+//  WalletNote
+//
+//  Created by 高野　泰生 on 2025/06/02.
+//
+
+import SwiftUI
+
+struct NumInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NumInputView()
+}
